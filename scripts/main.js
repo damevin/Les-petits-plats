5a962fc66dfd583d8c230f6fbf49f790b24a5266
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-07-19 14:51:32 +0200

    🔨  Create events listener on inputs

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,34 @@
-const recipesSection = document.querySelector('.results')
+const recipesSection = document.querySelector(".results");
+const globalSearchBar = document.querySelector(".research__bar__input");
+
+const ingredientInput = document.querySelector(".ingredient__input");
+const ingredientToggle = document.querySelector(".ingredient__button");
+const ingredientWrapper = document.querySelector(".ingredient__results");
+const ingredientChevron = document.querySelector(".ingredient__chevron");
+
+const apparatusInput = document.querySelector(".apparatus__input");
+const apparatusToggle = document.querySelector(".apparatus__button");
+const apparatusWrapper = document.querySelector(".apparatus__results");
+const apparatusChevron = document.querySelector(".apparatus__chevron");
+
+const ustensilsInput = document.querySelector(".ustensils__input");
+const ustensilsToggle = document.querySelector(".ustensils__button");
+const ustensilsWrapper = document.querySelector(".ustensils__results");
+const ustensilsChevron = document.querySelector(".ustensils__chevron");
+
+const generateFilters = (recipes) => {
+	let ingredients = [];
+	let apparatus = [];
+	let ustensils = [];
+	recipes.forEach((recipe) => {
+		ingredients = [
+			...new Set([...ingredients, ...recipe.ingredients.map((i) => i.ingredient.toLowerCase())]),
+		];
+		ustensils = [...new Set([...ustensils, ...recipe.ustensils.map((u) => u.toLowerCase())])];
+		apparatus = [...new Set([...apparatus, ...[recipe.appliance]])];
+	});
+	return { ingredients, ustensils, apparatus };
+};
 
 const getData = async () =>
 	await fetch("../scripts/data/recipes.json", {
@@ -12,17 +42,62 @@ const getData = async () =>
 		.then((res) => res.json())
 		.catch((err) => console.log("An error occurs when fetching recipes", err));
 
-
 const createRecipesCard = (recipes) => {
- recipes.forEach(recipe => {
-  recipesSection.append(new RecipeCard(recipe).recipeCard)
- });
-}
+	recipes.forEach((recipe) => {
+		recipesSection.append(new RecipeCard(recipe).recipeCard);
+	});
+};
 
 const init = async () => {
 	const { recipes } = await getData();
- createRecipesCard(recipes)
- console.log(recipes)
+	const { ingredients, ustensils, apparatus } = generateFilters(recipes);
+	generateFilters(recipes);
+	createRecipesCard(recipes);
+
+	/**
+	 * Affichage des ustensils au clique sur le toggle
+	 */
+	ustensilsToggle.addEventListener("click", () => {
+		if (ustensilsChevron.classList.contains("fa-chevron-down")) {
+			ustensilsChevron.classList.replace("fa-chevron-down", "fa-chevron-up");
+			ustensils.forEach((ustensil) => {
+				ustensilsWrapper.innerHTML += `<li>${ustensil}</li>`;
+			});
+		} else {
+			ustensilsChevron.classList.replace("fa-chevron-up", "fa-chevron-down");
+			ustensilsWrapper.innerHTML = "";
+		}
+	});
+
+	/**
+	 * Affichage des ingredients au clique sur le toggle
+	 */
+	ingredientToggle.addEventListener("click", () => {
+		if (ingredientChevron.classList.contains("fa-chevron-down")) {
+			ingredientChevron.classList.replace("fa-chevron-down", "fa-chevron-up");
+			ingredients.forEach((ingredient) => {
+				ingredientWrapper.innerHTML += `<li>${ingredient}</li>`;
+			});
+		} else {
+			ingredientChevron.classList.replace("fa-chevron-up", "fa-chevron-down");
+			ingredientWrapper.innerHTML = "";
+		}
+	});
+
+	/**
+	 * Affichage des appareils au clique sur le toggle
+	 */
+	apparatusToggle.addEventListener("click", () => {
+		if (apparatusChevron.classList.contains("fa-chevron-down")) {
+			apparatusChevron.classList.replace("fa-chevron-down", "fa-chevron-up");
+			apparatus.forEach((apparatus) => {
+				apparatusWrapper.innerHTML += `<li>${apparatus}</li>`;
+			});
+		} else {
+			apparatusChevron.classList.replace("fa-chevron-up", "fa-chevron-down");
+			apparatusWrapper.innerHTML = "";
+		}
+	});
 };
 
 init();
