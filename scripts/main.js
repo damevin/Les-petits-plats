879869bad0be6f3dec35421519344aae2cceb097
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-07-22 11:56:49 +0200

    🔨  Create filter for searchBar

--- scripts/main.js
+++ scripts/main.js
@@ -24,18 +24,43 @@ const getData = async () =>
 		.then((res) => res.json())
 		.catch((err) => console.log("An error occurs when fetching recipes", err));
 
+/**
+	* Create recipes card with a constructor
+	* @param {Array | Object} recipes 
+	*/
 const createRecipesCard = (recipes) => {
 	recipes.forEach((recipe) => {
 		recipesSection.append(new RecipeCard(recipe).recipeCard);
 	});
 };
 
+/**
+	* Filter query and display recipes card matching with query
+	* @param {Array | Object} recipes 
+	* @param {HTMLElement} searchBar 
+	*/
+const filteredRecipes = (recipes, searchBar) => {
+	searchBar.addEventListener("keyup", (e) => {
+		if (e.target.value.length > 3) {
+			recipesSection.innerHTML = "";
+			const query = e.target.value.toLowerCase();
+			const results = recipes.filter((recipe) => {
+				return recipe.name.toLowerCase().startsWith(query) || recipe.description.toLowerCase().includes(query)
+			});
+			createRecipesCard(results)
+		} else if (e.target.value.length <= 3) {
+			recipesSection.innerHTML = "";
+			createRecipesCard(recipes);
+		}
+	});
+};
 
 const init = async () => {
 	const { recipes } = await getData();
 	generateFilters(recipes);
-	createRecipesCard(recipes);
-	listenOnInputs(recipes)
+	listenOnInputs(recipes);
+	createRecipesCard(recipes)
+	filteredRecipes(recipes, globalSearchBar);
 };
 
 init();
