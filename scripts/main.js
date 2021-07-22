b979597138cd90dd273c0d6028322f5b385661b5
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-07-22 11:59:03 +0200

    ♻️  Remove search bar code

--- scripts/main.js
+++ scripts/main.js
@@ -34,26 +34,6 @@ const createRecipesCard = (recipes) => {
 	});
 };
 
-/**
-	* Filter query and display recipes card matching with query
-	* @param {Array | Object} recipes 
-	* @param {HTMLElement} searchBar 
-	*/
-const filteredRecipes = (recipes, searchBar) => {
-	searchBar.addEventListener("keyup", (e) => {
-		if (e.target.value.length > 3) {
-			recipesSection.innerHTML = "";
-			const query = e.target.value.toLowerCase();
-			const results = recipes.filter((recipe) => {
-				return recipe.name.toLowerCase().startsWith(query) || recipe.description.toLowerCase().includes(query)
-			});
-			createRecipesCard(results)
-		} else if (e.target.value.length <= 3) {
-			recipesSection.innerHTML = "";
-			createRecipesCard(recipes);
-		}
-	});
-};
 
 const init = async () => {
 	const { recipes } = await getData();
