6c69fc91b0729483f5407d7c3041967a18b4d9e6
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-07-27 16:17:38 +0200

    🚚  Rename file

--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -1,6 +1,10 @@
 /**
  * DOM elements
  */
+const selectedFilters = []
+
+const filtersBar = document.querySelector('.research__filters')
+
 const recipesSection = document.querySelector(".results");
 const globalSearchBar = document.querySelector(".research__bar__input");
 
@@ -8,13 +12,17 @@ const ingredientInput = document.querySelector(".ingredient__input");
 const ingredientToggle = document.querySelector(".ingredient__button");
 const ingredientWrapper = document.querySelector(".ingredient__results");
 const ingredientChevron = document.querySelector(".ingredient__chevron");
+const ingredientItem = document.querySelector(".ingredient__item")
 
 const apparatusInput = document.querySelector(".apparatus__input");
 const apparatusToggle = document.querySelector(".apparatus__button");
 const apparatusWrapper = document.querySelector(".apparatus__results");
 const apparatusChevron = document.querySelector(".apparatus__chevron");
+const apparatusItem = document.querySelector(".apparatus__item")
 
 const ustensilsInput = document.querySelector(".ustensils__input");
 const ustensilsToggle = document.querySelector(".ustensils__button");
 const ustensilsWrapper = document.querySelector(".ustensils__results");
 const ustensilsChevron = document.querySelector(".ustensils__chevron");
+const ustensilsItem = document.querySelector('.ustensils__item')
+
