23ad516c991110902b00f03c58a0841637dba07e
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-07-30 15:42:13 +0200

    🎨 Add no result message

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -1,21 +1,32 @@
-
 /**
-	* Filter query and display recipes card matching with query
-	* @param {Array | Object} recipes 
-	* @param {HTMLElement} searchBar 
-	*/
- const filteredRecipes = (recipes, searchBar) => {
-  searchBar.addEventListener("keyup", (e) => {
-   if (e.target.value.length > 3) {
-    recipesSection.innerHTML = "";
-    const query = e.target.value.toLowerCase();
-    const results = recipes.filter((recipe) => {
-     return recipe.name.toLowerCase().startsWith(query) || recipe.description.toLowerCase().includes(query)
-    });
-    createRecipesCard(results)
-   } else if (e.target.value.length <= 3) {
-    recipesSection.innerHTML = "";
-    createRecipesCard(recipes);
-   }
-  });
- };
+ * Filter query and display recipes card matching with query
+ * @param {Array | Object} recipes
+ * @param {HTMLElement} searchBar
+ */
+const filteredRecipes = (recipes, searchBar) => {
+	searchBar.addEventListener("keyup", (e) => {
+		if (e.target.value.length > 3) {
+			recipesSection.innerHTML = "";
+			const query = e.target.value.toLowerCase();
+			const results = recipes.filter((recipe) => {
+				return (
+					recipe.name.toLowerCase().startsWith(query) || recipe.description.toLowerCase().includes(query)
+				);
+			});
+			createRecipesCard(results);
+			if (!results.length) {
+				recipesSection.append(
+					createDom(
+						"div",
+						`Aucune recette ne correspond à votre critère… vous pouvez
+     chercher « tarte aux pommes », « poisson », etc.`,
+						{ class: "no__results" }
+					)
+				);
+			}
+		} else if (e.target.value.length <= 3) {
+			recipesSection.innerHTML = "";
+			createRecipesCard(recipes);
+		}
+	});
+};
