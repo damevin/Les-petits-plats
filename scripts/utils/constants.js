2b2cad32f02b4713852b3e44f1b5a9a5005d2621
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-07-30 13:54:03 +0200

    🌱 Update constants

--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -2,28 +2,30 @@
  * DOM elements
  */
 
-const selectedFilters = []
+const selectedFilters = [];
 
-const filtersBar = document.querySelector('.research__filters')
+const filtersBar = document.querySelector(".research__filters");
 
-const recipesSection = document.querySelector(".results");
 const globalSearchBar = document.querySelector(".research__bar__input");
+const recipesSection = document.querySelector(".results");
 
+const ingredientChevron = document.querySelector(".ingredient__chevron");
+const ingredientForm = document.querySelector(".ingredient__form");
 const ingredientInput = document.querySelector(".ingredient__input");
+const ingredientItem = document.querySelector(".ingredient__item");
 const ingredientToggle = document.querySelector(".ingredient__button");
 const ingredientWrapper = document.querySelector(".ingredient__results");
-const ingredientChevron = document.querySelector(".ingredient__chevron");
-const ingredientItem = document.querySelector(".ingredient__item")
 
+const apparatusChevron = document.querySelector(".apparatus__chevron");
+const apparatusForm = document.querySelector(".apparatus__form");
 const apparatusInput = document.querySelector(".apparatus__input");
+const apparatusItem = document.querySelector(".apparatus__item");
 const apparatusToggle = document.querySelector(".apparatus__button");
 const apparatusWrapper = document.querySelector(".apparatus__results");
-const apparatusChevron = document.querySelector(".apparatus__chevron");
-const apparatusItem = document.querySelector(".apparatus__item")
 
+const ustensilsChevron = document.querySelector(".ustensils__chevron");
+const ustensilsForm = document.querySelector(".ustensils__form");
 const ustensilsInput = document.querySelector(".ustensils__input");
+const ustensilsItem = document.querySelector(".ustensils__item");
 const ustensilsToggle = document.querySelector(".ustensils__button");
 const ustensilsWrapper = document.querySelector(".ustensils__results");
-const ustensilsChevron = document.querySelector(".ustensils__chevron");
-const ustensilsItem = document.querySelector('.ustensils__item')
-
