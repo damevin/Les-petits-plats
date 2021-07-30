999652b11611186def3bf756feab3b0e3c2cc339
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-07-30 12:10:50 +0200

    🚧 WIP: Remove filters in array for reload query

--- scripts/utils/filtersBar.js
+++ scripts/utils/filtersBar.js
@@ -1,14 +1,36 @@
-const updateFiltersBar = (selectedFilters, filterQuery) => {
+const updateFiltersBar = (selectedFiltersUnduplicated, recipes) => {
 	filtersBar.innerHTML = "";
-	selectedFilters.forEach((filter) => {
-		filtersBar.innerHTML += `<p class="filter__query">${filter}</p>`;
-  const filterQuery = document.querySelector('.filter__query');
-  listenOnFiltersBar(filterQuery)
+
+	selectedFiltersUnduplicated.forEach((filter) => {
+		return filtersBar.append(
+			createDom(
+				"div",
+				`${filter}`,
+				{ class: "filter__query" },
+				createDom("i", { class: "fal fa-times-circle filter__query__icon" })
+			)
+		);
+	});
+
+	const filterQuery = document.querySelectorAll(".filter__query");
+	const filters = Array.from(filterQuery);
+
+	const result = recipes.filter((recipe) => {
+		return filters.every((item) => {
+			return recipe.ingredients.some((i) => {
+				return i.ingredient.toLowerCase().includes(item.textContent.toLowerCase());
+			});
+		});
+	});
+
+	filterQuery.forEach((filter) => {
+		filter.addEventListener("click", () => {
+			const array = [...filterQuery];
+			const index = array.indexOf(filter);
+			array.splice(index, 1)
+			console.log("===============");
+			console.log(array);
+			console.log("===============");
+		});
 	});
 };
-
-const listenOnFiltersBar = (filterQuery) => {
- filterQuery.forEach((filter) => {
-  console.log(filter)
- })
-}
