e89e3969da775d83e9664dc7f1e93bb481242bd1
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-08-03 13:33:52 +0200

    ♻️  Improve format

--- scripts/utils/dom.js
+++ scripts/utils/dom.js
@@ -9,13 +9,13 @@ const createDom = (tag, ...childs) => {
  const element = document.createElement(tag)
  childs.forEach(child => {
   if (typeof child === 'string') {
-   const textNode = document.createTextNode(child)
+   const textNode = document.createTextNode(child);
    element.append(textNode)
   } else if (child instanceof HTMLElement) {
-   element.append(child)
+   element.append(child);
   } else if (child instanceof Object) {
    Object.entries(child).forEach(([key, value]) => {
-    element.setAttribute(key, value)
+    element.setAttribute(key, value);
    })
   }
  })
