eefb995d2e4fb9d4338cd577e5c3b3024232de59
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-08-05 18:10:07 +0200

    🔥  Second algorithm for general research

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -1,28 +1,35 @@
-/**
- * Filter query and display recipes card matching with query
- * @param {Array | Object} recipes
- * @param {HTMLElement} searchBar
- */
+
 const filteredRecipes = (recipes, searchBar) => {
 	searchBar.addEventListener("keyup", (e) => {
 		if (e.target.value.length >= 3) {
+			const results = [];
 			recipesSection.innerHTML = "";
 			const query = e.target.value.toLowerCase();
-			const results = recipes.filter((recipe) => {
-				return (
-					recipe.name.toLowerCase().startsWith(query) || recipe.description.toLowerCase().includes(query) || recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(query))
-				);
-			});
-			createRecipesCard(results);
-			if (!results.length) {
-				recipesSection.append(
-					createDom(
-						"div",
-						`Aucune recette ne correspond à votre critère… vous pouvez
-     chercher « tarte aux pommes », « poisson », etc.`,
-						{ class: "no__results" }
-					)
-				);
+			for (let i = 0; i < recipes.length; i++) {
+				const { name, ingredients, description } = recipes[i];
+				const includesInName = name.toLowerCase().includes(query);
+				const includesInDescription = description.toLowerCase().includes(query);
+				let includesInIngredients = false;
+				for (let y = 0; y < ingredients.length; y++)
+					if (ingredients[y].ingredient.toLowerCase().includes(query)) {
+						includesInIngredients = true;
+					}
+				if (includesInName || includesInDescription || includesInIngredients) {
+					results.push(recipes[i]);
+					recipesSection.innerHTML = "";
+					createRecipesCard(results)
+				}
+				if (!results.length) {
+					recipesSection.innerHTML = "";
+					recipesSection.append(
+						createDom(
+							"div",
+							`Aucune recette ne correspond à votre critère… vous pouvez
+						chercher « tarte aux pommes », « poisson », etc.`,
+							{ class: "no__results" }
+						)
+					);
+				}
 			}
 		} else if (e.target.value.length <= 3) {
 			recipesSection.innerHTML = "";
@@ -30,4 +37,3 @@ const filteredRecipes = (recipes, searchBar) => {
 		}
 	});
 };
-
