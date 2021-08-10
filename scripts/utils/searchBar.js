70d90a15e9be159950da6b43919e18bea7a5a0d1
Raphaël DAMEVIN <raphael.damevin@gmail.com>
2021-08-10 16:06:20 +0200

    🔨 Fix dom manipulation outside for loop

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -1,4 +1,3 @@
-
 const filteredRecipes = (recipes, searchBar) => {
 	searchBar.addEventListener("keyup", (e) => {
 		if (e.target.value.length >= 3) {
@@ -10,28 +9,34 @@ const filteredRecipes = (recipes, searchBar) => {
 				const includesInName = name.toLowerCase().includes(query);
 				const includesInDescription = description.toLowerCase().includes(query);
 				let includesInIngredients = false;
-				for (let y = 0; y < ingredients.length; y++)
+				for (let y = 0; y < ingredients.length; y++) {
 					if (ingredients[y].ingredient.toLowerCase().includes(query)) {
 						includesInIngredients = true;
 					}
+				}
 				if (includesInName || includesInDescription || includesInIngredients) {
 					results.push(recipes[i]);
-					recipesSection.innerHTML = "";
-					createRecipesCard(results)
-				}
-				if (!results.length) {
-					recipesSection.innerHTML = "";
-					recipesSection.append(
-						createDom(
-							"div",
-							`Aucune recette ne correspond à votre critère… vous pouvez
-						chercher « tarte aux pommes », « poisson », etc.`,
-							{ class: "no__results" }
-						)
-					);
 				}
 			}
-		} else if (e.target.value.length <= 3) {
+
+			if (results.length) {
+				recipesSection.innerHTML = "";
+				createRecipesCard(results);
+			}
+
+			if (!results.length) {
+				recipesSection.innerHTML = "";
+				recipesSection.append(
+					createDom(
+						"div",
+						`Aucune recette ne correspond à votre critère… vous pouvez
+						chercher « tarte aux pommes », « poisson », etc.`,
+						{ class: "no__results" }
+					)
+				);
+			}
+		}
+		if (e.target.value.length <= 3) {
 			recipesSection.innerHTML = "";
 			createRecipesCard(recipes);
 		}
